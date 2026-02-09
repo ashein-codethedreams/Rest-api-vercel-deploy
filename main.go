8deb4eb735881d7103c422ed0251c8d109348130
ashein-codethedreams <heroglasses155@gmail.com>
2026-02-09 08:54:49 +0630

    run server

--- main.go
+++ main.go
@@ -1,1 +1,22 @@
 package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+type todo struct {
+	ID        string `json:"id"`
+	Item      string `json:"title"`
+	Completed bool   `json:"completed"`
+}
+
+var todos = []todo{
+	{ID: "1", Item: "Learn Go", Completed: false},
+	{ID: "2", Item: "Build a REST API", Completed: false},
+	{ID: "3", Item: "Become a Fullstack Developer", Completed: false},
+}
+
+func main() {
+	router := gin.Default()
+	router.Run("localhost:8080")
+}
